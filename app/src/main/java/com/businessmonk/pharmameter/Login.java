05325eaf2b25a02b34638f685233aabe02fde555
Ahmed Nader <ahmed.nader1994@gmail.com>
2016-04-26 16:49:47 +0200

    fix some issues

--- app/src/main/java/com/businessmonk/pharmameter/Login.java
+++ app/src/main/java/com/businessmonk/pharmameter/Login.java
@@ -1,8 +1,10 @@
 package com.businessmonk.pharmameter;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Toast;
@@ -29,5 +31,10 @@ public class Login extends Activity {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             window.setStatusBarColor(this.getResources().getColor(R.color.colorPrimaryDark));
         }
+    }
+
+    public void signup(View view) {
+        Intent intent = new Intent(this,Signup.class);
+        startActivity(intent);
     }
 }
